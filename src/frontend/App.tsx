58c40a82cd0bbf7e224aaa0cbe71f422abf1936a
Rental Management System <rental.system@example.com>
2025-12-24 09:52:02 -0300

    Migrate frontend to Material-UI with custom light/dark themes

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Container, Box } from '@mui/material';
 import Home from './pages/Home';
 import Properties from './pages/Properties';
 import Reservations from './pages/Reservations';
 import Navbar from './components/Navbar';
-import './styles.css';
 import { API_ENDPOINTS } from './config';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Navbar />
-      <div className="container mt-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/properties" element={<Properties />} />
-          <Route path="/reservations" element={<Reservations />} />
-        </Routes>
-      </div>
+      <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <Navbar />
+        <Container maxWidth="lg" sx={{ flexGrow: 1, py: 4 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/properties" element={<Properties />} />
+            <Route path="/reservations" element={<Reservations />} />
+          </Routes>
+        </Container>
+      </Box>
     </Router>
   );
 };
